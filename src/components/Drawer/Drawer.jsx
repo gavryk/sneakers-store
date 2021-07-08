860d7a42d573e49fb5606def1447c1c5a5ea9769
Oleg <gvozdyk23@gmail.com>
2021-07-09 02:55:32 +0300

    remove sneakers from cart

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -1,5 +1,6 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faTimes} from "@fortawesome/free-solid-svg-icons";
+import emptyImg from '../../img/empty-cart.png';
 
 function Drawer({ onClose, onRemoveItem, items = [] }) {
     return (
@@ -12,45 +13,57 @@ function Drawer({ onClose, onRemoveItem, items = [] }) {
                     </button>
                 </div>
 
-                <div className="cart-items flex">
-                    {
-                        items.map(item => {
-                            return (
-                                <div className="cart-item p-15 d-flex align-center justify-between mb-20">
-                                    <img
-                                        className='mr-10 cartItemImage'
-                                        width={70}
-                                        height={70}
-                                        src={ item.image }
-                                        alt="Sneaker"/>
-                                    <div className='mr-10'>
-                                        <h5 className='mb-5'>{ item.title }</h5>
-                                        <b>{ item.price }$</b>
-                                    </div>
-                                    <button className="button removeBtn" onClick={ () => onRemoveItem(item.id) }>
-                                        <FontAwesomeIcon icon={faTimes} />
-                                    </button>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-
-                <div className='cartTotalBlock'>
-                    <ul className='mb-20'>
-                        <li>
-                            <span>Total: </span>
-                            <div></div>
-                            <b>120$</b>
-                        </li>
-                        <li>
-                            <span>Tax 5%: </span>
-                            <div></div>
-                            <b>20$</b>
-                        </li>
-                    </ul>
-                    <button className='btn greenBtn'>To Order</button>
-                </div>
+                {
+                    items.length > 0
+                        ? <>
+                            <div className="cart-items d-flex justify-between flex-column">
+                                {
+                                    items.map(item => {
+                                        return (
+                                            <div className="cart-item p-15 d-flex align-center justify-between mb-20">
+                                                <img
+                                                    className='mr-10 cartItemImage'
+                                                    width={70}
+                                                    height={70}
+                                                    src={ item.image }
+                                                    alt="Sneaker"/>
+                                                <div className='mr-10'>
+                                                    <h5 className='mb-5'>{ item.title }</h5>
+                                                    <b>{ item.price }$</b>
+                                                </div>
+                                                <button className="button removeBtn" onClick={ () => onRemoveItem(item.id) }>
+                                                    <FontAwesomeIcon icon={faTimes} />
+                                                </button>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                            <div className='cartTotalBlock'>
+                                <ul className='mb-20'>
+                                    <li>
+                                        <span>Total: </span>
+                                        <div></div>
+                                        <b>120$</b>
+                                    </li>
+                                    <li>
+                                        <span>Tax 5%: </span>
+                                        <div></div>
+                                        <b>20$</b>
+                                    </li>
+                                </ul>
+                                <button className='btn greenBtn'>To Order</button>
+                            </div>
+                        </>
+                        : <div className="cart-empty d-flex justify-center align-center">
+                            <div>
+                                <img src={ emptyImg } alt="empty"/>
+                                <h1>Cart Empty</h1>
+                                <p>Add at least one pair of sneakers to order.</p>
+                                <button onClick={ onClose } className='btn greenBtn'>Back To Shop</button>
+                            </div>
+                        </div>
+                }
             </div>
         </div>
     );
